d0536f9c35eb0b42f2671403917459c0bd81ec40
hmj <hmj@itcast.cn>
2015-12-10 14:23:14 +0800

    Initial Commit

---
+++ Masonry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Masonry
+//
+//  Created by 惠绵金 on 15/12/10.
+//  Copyright © 2015年 huimianjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Masonry/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Masonry
+//
+//  Created by 惠绵金 on 15/12/10.
+//  Copyright © 2015年 huimianjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
